e97e2b3a9d083529558c82fef1bb5a06e996c977
Ralph Martinez <rafael.adan.martinez@gmail.com>
2015-04-26 17:50:22 -0600

    Setting debugging mode to true if not set in config struct by config file

--- genever.go
+++ genever.go
@@ -47,7 +47,9 @@ func (b *bartender) initAction(c *cli.Context) {
 	}
 	
 	// Set debugging flag, on by default
-	
+	if b.config.Debugging == nil {
+		b.config.Debugging = true
+	}
 	if c.IsSet("debugging") {
 		b.config.Debugging = true
 	}
