d6a5a79af2a256eb6ddd5c9ba17499bdc3776cbd
Ralph Martinez <rafael.adan.martinez@gmail.com>
2015-04-25 22:09:23 -0600

    removing goroutine from genever

--- bartender.go
+++ bartender.go
@@ -112,7 +112,7 @@ func (b *bartender) Start(args []string) {
 		case "production", "prod", "p":
 			b.server.Run(":8989")
 		case "development", "dev", "d":
-			go b.app.Run(args)
+			b.app.Run(args)
 			//b.server.Run(":9191")
 	}
 }
