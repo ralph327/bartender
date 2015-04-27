eed46cf1df3a7d59289c01501fc67b2f16343da5
Ralph Martinez <rafael.adan.martinez@gmail.com>
2015-04-26 20:29:45 -0600

    Add logic to copy /views and /public from framework into web app's working directory if those respective directories do not already exist

--- init.go
+++ init.go
@@ -3,6 +3,7 @@ package bartender
 import (
 	"github.com/codegangsta/cli"
 	"github.com/gin-gonic/gin"
+	"github.com/codegangsta/envy/lib"
 	"net/http"
 	"time"
 	"fmt"
@@ -93,8 +94,56 @@ func (b *bartender) Init(configPath string) {
 	b.initiated  = false
 	
 	// Initialize folders for web app
+	gopath := envy.MustGet("GOPATH")
+	
+	// Get the working directory
 	wd, _ := os.Getwd()
-	fmt.Fprintf(os.Stderr,"pwd: %s\n", wd)
+	
+	// Copy framework views to the working directory
+	// if the directory does not already exist
+	if !dirExists(wd + "/views") {
+		fmt.Fprintf(os.Stdout, "Copying /views to working directory")
+		err = copyFile(gopath + "/src/github.com/ralph327/bartender/views", wd + "/views")
+		
+		if err != nil {
+			fmt.Fprintf(os.Stderr,"Error while copying /views: %s\n", err)
+			os.Exit(1)
+		}
+	}
+	
+	// Copy framework views to the working directory
+	// if the directory does not already exist
+	if !dirExists(wd + "/public") {
+		fmt.Fprintf(os.Stdout, "Copying /public to working directory")
+		err = copyFile(gopath + "/src/github.com/ralph327/bartender/public", wd + "/public")
+		
+		if err != nil {
+			fmt.Fprintf(os.Stderr,"Error while copying /public: %s\n", err)
+			os.Exit(1)
+		}
+	}
+}
+
+func dirExists(dir string) bool {
+	// check if dir exists
+	src,err := os.Stat(dir)
+	
+	
+	if err == nil {
+		if !src.IsDir() {
+			fmt.Fprintf(os.Stderr, "%s is not a directory", dir)
+			os.Exit(1)
+		}
+		return true
+	}
+	
+	// Check if err is due to not exist
+	if os.IsNotExist(err) { 
+		return false
+	}
+	
+	// Default to false with errors
+	return false
 }
 
  func copyFile(source string, dest string) (err error) {
@@ -123,6 +172,7 @@ func (b *bartender) Init(configPath string) {
      return
  }
 
+// Recursively copy source to dest
  func copyDir(source string, dest string) (err error) {
 
      // get properties of source dir
