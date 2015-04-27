4fb4dce9d9f303621af144121af07c0e2d797d9c
Ralph Martinez <rafael.adan.martinez@gmail.com>
2015-04-26 19:54:23 -0600

    Created init.go to separate more complex logic for init only

--- bartender.go
+++ bartender.go
@@ -6,10 +6,7 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/suapapa/go_sass"
 	"log"
-	"os"
-	"fmt"
 	"time"
-	"net/http"
 )
 
 type Bartender interface {
@@ -36,88 +33,7 @@ type bartender struct {
 	initiated   bool
 }
 
-func (b *bartender) Init(configPath string) {
-	var err error
-	
-	// Initialize the cli app
-	b.app = cli.NewApp()
-	
-	// load the configuration
-	b.config, err = loadConfig(configPath)
-	
-	if err != nil {
-		fmt.Fprintf(os.Stderr,"Error while loading config: %s\n", err)
-		os.Exit(1)
-	}
-	
-	// Set application data
-	b.app.Name = b.config.SiteName
-	b.app.Usage = "Bartender is a framework with a hot reload utility baked in. This is disabled on production environments."
-	b.app.Action = b.mainAction
-	b.app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "env,e",
-			Value: "d",
-			Usage: "environment to run server under",
-		},
-		cli.StringFlag{
-			Name:  "debugging,d",
-			Value: "t",
-			Usage: "Output debugging info or not",
-		},
-		cli.StringFlag{
-			Name:  "proxyPort,p",
-			Value: "9000",
-			Usage: "port for the proxy server",
-		},
-		cli.StringFlag{
-			Name:  "appPort,a",
-			Value: "9001",
-			Usage: "port for the Go web server",
-			EnvVar: "PORT",
-		},
-		cli.StringFlag{
-			Name:  "bin,b",
-			Value: b.config.SiteName,
-			Usage: "name of generated binary file",
-		},
-		cli.StringFlag{
-			Name:  "path,t",
-			Value: ".",
-			Usage: "Path to watch files from",
-		},
-		cli.BoolFlag{
-			Name:  "godep,g",
-			Usage: "use godep when building",
-		},
-	}
-	b.app.Commands = []cli.Command{
-		{
-			Name:	 "init",
-			ShortName: "i",
-			Usage:	 "Initiate bartender",
-			Action:	 b.initAction,
-		},
-		{
-			Name:      "run",
-			ShortName: "r",
-			Usage:     "Run the genever proxy in the current working directory",
-			Action:    b.mainAction,
-		},
-		{
-			Name:      "env",
-			ShortName: "e",
-			Usage:     "Display environment variables set by the .env file",
-			Action:    b.envAction,
-		},
-	}	
-	b.server = gin.Default()
-	b.server.GET("/", func(c *gin.Context) {
-        c.String(http.StatusOK, b.config.Put)
-    })
-	b.startTime  = time.Now()
-	b.initiated  = false
-}
+
 
 func (b *bartender) Start(args []string) {	
 	// Run mainAction
---
+++ init.go
@@ -0,0 +1,166 @@
+package bartender 
+
+import (
+	"github.com/codegangsta/cli"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+	"fmt"
+	"os"
+	"io"
+)
+
+func (b *bartender) Init(configPath string) {
+	var err error
+	
+	// Initialize the cli app
+	b.app = cli.NewApp()
+	
+	// load the configuration
+	b.config, err = loadConfig(configPath)
+	
+	if err != nil {
+		fmt.Fprintf(os.Stderr,"Error while loading config: %s\n", err)
+		os.Exit(1)
+	}
+	
+	// Set application data
+	b.app.Name = b.config.SiteName
+	b.app.Usage = "Bartender is a framework with a hot reload utility baked in. This is disabled on production environments."
+	b.app.Action = b.mainAction
+	b.app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "env,e",
+			Value: "d",
+			Usage: "environment to run server under",
+		},
+		cli.StringFlag{
+			Name:  "debugging,d",
+			Value: "t",
+			Usage: "Output debugging info or not",
+		},
+		cli.StringFlag{
+			Name:  "proxyPort,p",
+			Value: "9000",
+			Usage: "port for the proxy server",
+		},
+		cli.StringFlag{
+			Name:  "appPort,a",
+			Value: "9001",
+			Usage: "port for the Go web server",
+			EnvVar: "PORT",
+		},
+		cli.StringFlag{
+			Name:  "bin,b",
+			Value: b.config.SiteName,
+			Usage: "name of generated binary file",
+		},
+		cli.StringFlag{
+			Name:  "path,t",
+			Value: ".",
+			Usage: "Path to watch files from",
+		},
+		cli.BoolFlag{
+			Name:  "godep,g",
+			Usage: "use godep when building",
+		},
+	}
+	b.app.Commands = []cli.Command{
+		{
+			Name:	 "init",
+			ShortName: "i",
+			Usage:	 "Initiate bartender",
+			Action:	 b.initAction,
+		},
+		{
+			Name:      "run",
+			ShortName: "r",
+			Usage:     "Run the genever proxy in the current working directory",
+			Action:    b.mainAction,
+		},
+		{
+			Name:      "env",
+			ShortName: "e",
+			Usage:     "Display environment variables set by the .env file",
+			Action:    b.envAction,
+		},
+	}	
+	b.server = gin.Default()
+	b.server.GET("/", func(c *gin.Context) {
+        c.String(http.StatusOK, b.config.Put)
+    })
+	b.startTime  = time.Now()
+	b.initiated  = false
+	
+	// Initialize folders for web app
+	fmt.Fprintf(os.Stderr,"pwd: %s\n", os.Getwd)
+}
+
+ func copyFile(source string, dest string) (err error) {
+     sourcefile, err := os.Open(source)
+     if err != nil {
+         return err
+     }
+
+     defer sourcefile.Close()
+
+     destfile, err := os.Create(dest)
+     if err != nil {
+         return err
+     }
+
+     defer destfile.Close()
+
+     _, err = io.Copy(destfile, sourcefile)
+     if err == nil {
+         sourceinfo, err := os.Stat(source)
+         if err != nil {
+             err = os.Chmod(dest, sourceinfo.Mode())
+         }
+     }
+
+     return
+ }
+
+ func copyDir(source string, dest string) (err error) {
+
+     // get properties of source dir
+     sourceinfo, err := os.Stat(source)
+     if err != nil {
+         return err
+     }
+
+     // create dest dir
+     err = os.MkdirAll(dest, sourceinfo.Mode())
+     if err != nil {
+         return err
+     }
+
+     directory, _ := os.Open(source)
+
+     objects, err := directory.Readdir(-1)
+
+     for _, obj := range objects {
+
+         sourcefilepointer := source + "/" + obj.Name()
+
+         destinationfilepointer := dest + "/" + obj.Name()
+
+
+         if obj.IsDir() {
+             // create sub-directories - recursively
+             err = copyDir(sourcefilepointer, destinationfilepointer)
+             if err != nil {
+                 fmt.Println(err)
+             }
+         } else {
+             // perform copy
+             err = copyFile(sourcefilepointer, destinationfilepointer)
+             if err != nil {
+                 fmt.Println(err)
+             }
+         }
+
+     }
+     return
+ }
