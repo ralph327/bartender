9cdd832a92087a645ba1322b9b5268a3918e3e68
Ralph Martinez <rafael.adan.martinez@gmail.com>
2015-05-26 22:40:46 -0600

    invoker debugging

--- invoker.go
+++ invoker.go
@@ -2,12 +2,15 @@ package bartender
 
 import (
 	"reflect"
+	"fmt"
 )
 
 func (c * Controller) methodInvoker(method string) {	
 	argsValue := make([]reflect.Value, len(c.Args))
 	
 	for i, elem := range c.Args {argsValue[i] = reflect.ValueOf(elem)} 
+
+	fmt.Println(method, " - ", argsValue)
 
 	// Call action with args
 	//c.controllerValue.MethodByName(method).Call(argsValue)
