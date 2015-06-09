e7348a88617c2ab96c4358c291d9f42827473904
Ralph Martinez <rafael.adan.martinez@gmail.com>
2015-06-08 21:13:54 -0600

    debugging

--- controller.go
+++ controller.go
@@ -73,7 +73,7 @@ func validateController(controller interface{}, parentControllerType reflect.Typ
 		}
 	}
 		
-	controllerType := reflect.TypeOf(controller)
+	controllerType = reflect.TypeOf(controller)
 	
 	return controllerType
 }
